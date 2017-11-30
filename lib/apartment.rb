c11724f5b688a122f77e222fad5148ff5c0c0dc7
Liang-Chi Tseng <lctseng@cs.nctu.edu.tw>
2017-11-30 12:55:07 +0800

    callbacks around migrate

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -24,7 +24,7 @@ module Apartment
     extend Forwardable
 
     ACCESSOR_METHODS  = [:use_schemas, :use_sql, :seed_after_create, :prepend_environment, :append_environment, :with_multi_server_setup, :use_parallel_tenant_task, :use_single_schema, :single_schema_default_tenant ]
-    WRITER_METHODS    = [:tenant_names, :database_schema_file, :excluded_models, :default_schema, :persistent_schemas, :connection_class, :tld_length, :db_migrate_tenants, :seed_data_file, :num_parallel_in_processes, :multi_tenant_models, :partition_model, :compute_tenant_id_method, :compute_tenant_name_method, :single_schema_partition_field, :schema_exist_check_method]
+    WRITER_METHODS    = [:tenant_names, :database_schema_file, :excluded_models, :default_schema, :persistent_schemas, :connection_class, :tld_length, :db_migrate_tenants, :seed_data_file, :num_parallel_in_processes, :multi_tenant_models, :partition_model, :compute_tenant_id_method, :compute_tenant_name_method, :single_schema_partition_field, :schema_exist_check_method, :before_migrating_tenant_method, :after_migrating_tenant_method]
 
     attr_accessor(*ACCESSOR_METHODS)
     attr_writer(*WRITER_METHODS)
@@ -189,6 +189,16 @@ module Apartment
     def schema_exist_check_method 
       @schema_exist_check_method || Proc.new do |tenant| 
         Apartment.connection.schema_exists? tenant 
+      end 
+    end 
+
+    def before_migrating_tenant_method 
+      @before_migrating_tenant_method || Proc.new do |tenant_nane| 
+      end 
+    end 
+ 
+    def after_migrating_tenant_method 
+      @after_migrating_tenant_method || Proc.new do |tenant_nane| 
       end 
     end 
 
