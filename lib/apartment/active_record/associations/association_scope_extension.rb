08e2e78f05d39875252bebe1bf774066cab63378
svgiridharreddy <giridhar.reddy@goshopmatic.com>
2022-06-20 12:58:23 +0530

    updated add_constriants methods according to rails 5

--- lib/apartment/active_record/associations/association_scope_extension.rb
+++ lib/apartment/active_record/associations/association_scope_extension.rb
@@ -1,52 +1,34 @@
 module ActiveRecord
   module Associations
     class AssociationScope #:nodoc:
-      def add_constraints(scope, owner, assoc_klass, refl, tracker)
-        # Shopmatic: handle 'has_many': add owner multi tenant id 
-        if owner.class&.multi_tenant? && assoc_klass&.multi_tenant?
-          scope = scope.where(tenant_id: owner.tenant_id)
+
+      def add_constraints(scope, owner, chain)
+        scope = last_chain_scope(scope, chain.last, owner)
+
+        chain.each_cons(2) do |reflection, next_reflection|
+          scope = next_chain_scope(scope, reflection, next_reflection)
         end
-        # ===
-        chain = refl.chain
-        scope_chain = refl.scope_chain
 
-        tables = construct_tables(chain, assoc_klass, refl, tracker)
-
-        owner_reflection = chain.last
-        table = tables.last
-        scope = last_chain_scope(scope, table, owner_reflection, owner, tracker, assoc_klass)
-
-        chain.each_with_index do |reflection, i|
-          table, foreign_table = tables.shift, tables.first
-
-          unless reflection == chain.last
-            next_reflection = chain[i + 1]
-            scope = next_chain_scope(scope, table, reflection, tracker, assoc_klass, foreign_table, next_reflection)
-          end
-
-          is_first_chain = i == 0
-          klass = is_first_chain ? assoc_klass : reflection.klass
-
+        chain_head = chain.first
+        chain.reverse_each do |reflection|
           # Exclude the scope of the association itself, because that
           # was already merged in the #scope method.
-          scope_chain[i].each do |scope_chain_item|
-            item  = eval_scope(klass, scope_chain_item, owner)
+          reflection.constraints.each do |scope_chain_item|
+            item = eval_scope(reflection, scope_chain_item, owner)
 
-            if scope_chain_item == refl.scope
-              scope.merge! item.except(:where, :includes, :bind)
+            if scope_chain_item == chain_head.scope
+              scope.merge! item.except(:where, :includes, :unscope, :order)
             end
 
-            if is_first_chain
+            reflection.all_includes do
               scope.includes! item.includes_values
             end
 
             scope.unscope!(*item.unscope_values)
-            scope.where_values += item.where_values
-            scope.bind_values  += item.bind_values
-            scope.order_values |= item.order_values
+            scope.where_clause += item.where_clause
+            scope.order_values = item.order_values | scope.order_values
           end
         end
-
         scope
       end
     end
